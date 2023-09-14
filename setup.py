6445a1067fc249355b77bb74e28e5e90633dbbd6
Sven Hillenkoetter <ujytl@student.kit.edu>
2023-09-14 12:19:20 +0200

    install fixes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trust_region_layers',
+    version='0.1',
+    packages=find_packages(include=["trust_region_layers"]),
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
